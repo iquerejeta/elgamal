02244feb0aba0033977652a0d7d3da03958e2b21
gpestana <g6pestana@gmail.com>
2020-01-13 11:11:07 +0000

    Implements SecretKey serialization/deserialization

--- benches/elgamal-ristretto.rs
+++ benches/elgamal-ristretto.rs
@@ -2,108 +2,98 @@
 extern crate criterion;
 use criterion::Criterion;
 
+extern crate curve25519_dalek;
 extern crate elgamal_ristretto;
 extern crate rand;
-extern crate curve25519_dalek;
 
-use elgamal_ristretto::{PublicKey, SecretKey, };
-use rand_core::OsRng;
 use curve25519_dalek::ristretto::RistrettoPoint;
+use elgamal_ristretto::{PublicKey, SecretKey};
+use rand_core::OsRng;
 
 fn encrypt_ciphertext(c: &mut Criterion) {
     let label = format!("Encryption");
-    c.bench_function(
-        &label,
-        move |b| {
-            let mut csprng = OsRng;
-            let sk = SecretKey::new(&mut csprng);
-            let pk = PublicKey::from(&sk);
+    c.bench_function(&label, move |b| {
+        let mut csprng = OsRng;
+        let sk = SecretKey::new(&mut csprng);
+        let pk = PublicKey::from(&sk);
 
-            let ptxt = RistrettoPoint::random(&mut csprng);
+        let ptxt = RistrettoPoint::random(&mut csprng);
 
-            b.iter(|| {
-                pk.encrypt(ptxt);
-            })
-        }
-    );
+        b.iter(|| {
+            pk.encrypt(ptxt);
+        })
+    });
 }
 
 fn decrypt_ciphertext(c: &mut Criterion) {
     let label = format!("Decryption");
-    c.bench_function(
-        &label,
-        move |b| {
-            let mut csprng = OsRng;
-            let sk = SecretKey::new(&mut csprng);
-            let pk = PublicKey::from(&sk);
+    c.bench_function(&label, move |b| {
+        let mut csprng = OsRng;
+        let sk = SecretKey::new(&mut csprng);
+        let pk = PublicKey::from(&sk);
 
-            let ptxt = RistrettoPoint::random(&mut csprng);
-            let ctxt = pk.encrypt(ptxt);
+        let ptxt = RistrettoPoint::random(&mut csprng);
+        let ctxt = pk.encrypt(ptxt);
 
-            b.iter(|| {
-                sk.decrypt(ctxt);
-            })
-        }
-    );
+        b.iter(|| {
+            sk.decrypt(ctxt);
+        })
+    });
 }
 
 fn signature(c: &mut Criterion) {
     let label = format!("Signature");
-    c.bench_function(
-        &label,
-        move |b| {
-            let mut csprng = OsRng;
-            let sk = SecretKey::new(&mut csprng);
+    c.bench_function(&label, move |b| {
+        let mut csprng = OsRng;
+        let sk = SecretKey::new(&mut csprng);
 
-            let msg = RistrettoPoint::random(&mut csprng);
+        let msg = RistrettoPoint::random(&mut csprng);
 
-            b.iter(|| {
-                sk.sign(msg);
-            })
-        }
-    );
+        b.iter(|| {
+            sk.sign(msg);
+        })
+    });
 }
 
 fn verify_signature(c: &mut Criterion) {
     let label = format!("Verify Signature");
-    c.bench_function(
-        &label,
-        move |b| {
-            let mut csprng = OsRng;
-            let sk = SecretKey::new(&mut csprng);
-            let pk = PublicKey::from(&sk);
+    c.bench_function(&label, move |b| {
+        let mut csprng = OsRng;
+        let sk = SecretKey::new(&mut csprng);
+        let pk = PublicKey::from(&sk);
 
-            let msg = RistrettoPoint::random(&mut csprng);
-            let signature = sk.sign(msg);
+        let msg = RistrettoPoint::random(&mut csprng);
+        let signature = sk.sign(msg);
 
-            b.iter(|| {
-                pk.verify_signature(&msg, signature)
-            })
-        }
-    );
+        b.iter(|| pk.verify_signature(&msg, signature))
+    });
 }
 
 fn ciphertext_addition(c: &mut Criterion) {
     let label = format!("Ciphertext homomorphic addition");
-    c.bench_function(
-        &label,
-        move |b| {
-            let mut csprng = OsRng;
-            let sk = SecretKey::new(&mut csprng);
-            let pk = PublicKey::from(&sk);
+    c.bench_function(&label, move |b| {
+        let mut csprng = OsRng;
+        let sk = SecretKey::new(&mut csprng);
+        let pk = PublicKey::from(&sk);
 
-            let ptxt1 = RistrettoPoint::random(&mut csprng);
-            let ptxt2 = RistrettoPoint::random(&mut csprng);
+        let ptxt1 = RistrettoPoint::random(&mut csprng);
+        let ptxt2 = RistrettoPoint::random(&mut csprng);
 
-            let ctxt1 = pk.encrypt(ptxt1);
-            let ctxt2 = pk.encrypt(ptxt2);
+        let ctxt1 = pk.encrypt(ptxt1);
+        let ctxt2 = pk.encrypt(ptxt2);
 
-            b.iter(|| {
-                let _ = ctxt1 + ctxt2;
-            })
-        }
-    );
+        b.iter(|| {
+            let _ = ctxt1 + ctxt2;
+        })
+    });
 }
 
-criterion_group!(benches, encrypt_ciphertext, decrypt_ciphertext, signature, verify_signature, ciphertext_addition);
+criterion_group!(
+    benches,
+    encrypt_ciphertext,
+    decrypt_ciphertext,
+    signature,
+    verify_signature,
+    ciphertext_addition
+);
 criterion_main!(benches);
